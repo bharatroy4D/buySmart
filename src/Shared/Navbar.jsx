0a5f2626c180e16b95978223268708de9fa49636
bharatroy4D <bharatroy1005@gmail.com>
2025-10-25 19:56:58 +0600

    first commit

---
+++ src/Shared/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className='flex justify-between items-center' >
+                <h1>BuySmart</h1>
+                <div><input type="text" name="" id="" placeholder='Search...' /></div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
