7f3c13901291f1c33e1837a1d3e6f45af46319bb
bharatroy4D <bharatroy1005@gmail.com>
2025-11-05 12:12:04 +0600

    banner into aro icon set

--- src/Pages/Home/banner/Banner.jsx
+++ src/Pages/Home/banner/Banner.jsx
@@ -2,9 +2,33 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import banner_1 from "../../../assets/banner_img/Smartphone Banner.png";
 import banner_2 from "../../../assets/banner_img/smartwatch electronics.png";
 import banner_3 from "../../../assets/banner_img/Appliances Banner.png";
+
+// Custom Arrow Components
+const NextArrow = ({ onClick }) => {
+  return (
+    <div
+      className="absolute top-1/2 right-2 z-20 cursor-pointer text-white text-xl transform -translate-y-1/2"
+      onClick={onClick}
+    >
+      <FaArrowRight />
+    </div>
+  );
+};
+
+const PrevArrow = ({ onClick }) => {
+  return (
+    <div
+      className="absolute top-1/2 left-2 z-20 cursor-pointer text-white text-xl transform -translate-y-1/2"
+      onClick={onClick}
+    >
+      <FaArrowLeft />
+    </div>
+  );
+};
 
 const Banner = () => {
   const settings = {
@@ -14,32 +38,34 @@ const Banner = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 3000,
     pauseOnHover: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
   };
 
   return (
-    <div className="banner-slider">
+    <div className="w-full overflow-hidden relative">
       <Slider {...settings}>
         <div>
-          <img 
-            src={banner_1} 
-            alt="Smartphone Banner" 
-            className="w-full h-[50%]"
+          <img
+            src={banner_1}
+            alt="Smartphone Banner"
+            className="w-full  h-[60vh] object-cover"
           />
         </div>
         <div>
-          <img 
-            src={banner_2} 
-            alt="Smartwatch Banner" 
-            className="w-full h-[50%]"
+          <img
+            src={banner_2}
+            alt="Smartwatch Banner"
+            className="w-full h-[60vh] object-cover"
           />
         </div>
         <div>
-          <img 
-            src={banner_3} 
-            alt="Appliances Banner" 
-            className="w-full h-[50%] "
+          <img
+            src={banner_3}
+            alt="Appliances Banner"
+            className="w-ful h-[60vh] object-cover"
           />
         </div>
       </Slider>
