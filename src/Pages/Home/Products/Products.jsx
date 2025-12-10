6f3f9a7eea5d6bb9ab29063b2080bccb0f1c5c90
bharatroy4D <bharatroy1005@gmail.com>
2025-12-10 14:44:07 +0600

    Products of category code edit

--- src/Pages/Home/Products/Products.jsx
+++ src/Pages/Home/Products/Products.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import SideBar from './SideBar';
-import MainProducts from './MainProducts';
+import React, { useState } from "react";
+import SideBar from "./SideBar";
+import MainProducts from "./MainProducts";
 
 const Products = () => {
-      const [category, setCategory] = useState("All Categories");
+  const [category, setCategory] = useState("All Categories");
 
-    return (
-        <div className="container mx-auto px-5 py-8 flex flex-col md:flex-row gap-8">
-            <SideBar category={category} setCategory={setCategory} />
-            <MainProducts category={category} />
-        </div>
-    );
+  return (
+    <div className="container mx-auto px-5 py-8 flex flex-col md:flex-row gap-8">
+      <SideBar category={category} setCategory={setCategory} />
+      <MainProducts category={category} />
+    </div>
+  );
 };
 
 export default Products;
