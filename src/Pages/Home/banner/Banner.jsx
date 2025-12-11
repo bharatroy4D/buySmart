d889090c2b915f5bc70e0341045c4e2fd1b278eb
bharatroy4D <bharatroy1005@gmail.com>
2025-12-11 10:59:40 +0600

    Banner is responsive complited

--- src/Pages/Home/banner/Banner.jsx
+++ src/Pages/Home/banner/Banner.jsx
@@ -3,38 +3,39 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
 import banner_1 from "../../../assets/banner_img/Smartphone Banner.png";
 import banner_2 from "../../../assets/banner_img/smartwatch electronics.png";
 import banner_3 from "../../../assets/banner_img/Appliances Banner.png";
 
-// Custom Arrow Components
-const NextArrow = ({ onClick }) => {
-  return (
-    <div
-      className="absolute top-1/2 right-2 z-20 cursor-pointer text-white text-xl transform -translate-y-1/2"
-      onClick={onClick}
-    >
-      <FaArrowRight />
-    </div>
-  );
-};
+// Custom Arrow
+const NextArrow = ({ onClick }) => (
+  <div
+    className="absolute top-1/2 right-2 md:right-5 z-20 cursor-pointer 
+               text-white text-lg md:text-2xl transform -translate-y-1/2 
+               bg-black/40 p-2 rounded-full hover:bg-black/60 transition"
+    onClick={onClick}
+  >
+    <FaArrowRight />
+  </div>
+);
 
-const PrevArrow = ({ onClick }) => {
-  return (
-    <div
-      className="absolute top-1/2 left-2 z-20 cursor-pointer text-white text-xl transform -translate-y-1/2"
-      onClick={onClick}
-    >
-      <FaArrowLeft />
-    </div>
-  );
-};
+const PrevArrow = ({ onClick }) => (
+  <div
+    className="absolute top-1/2 left-2 md:left-5 z-20 cursor-pointer 
+               text-white text-lg md:text-2xl transform -translate-y-1/2 
+               bg-black/40 p-2 rounded-full hover:bg-black/60 transition"
+    onClick={onClick}
+  >
+    <FaArrowLeft />
+  </div>
+);
 
 const Banner = () => {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 600,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
@@ -42,30 +43,40 @@ const Banner = () => {
     pauseOnHover: true,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 768, // Mobile
+        settings: {
+          arrows: false, // hide arrows on mobile
+        },
+      },
+    ],
   };
 
   return (
-    <div className="w-full overflow-hidden relative">
+    <div className="w-full relative">
       <Slider {...settings}>
         <div>
           <img
             src={banner_1}
             alt="Smartphone Banner"
-            className="w-full  h-[60vh] object-cover"
+            className="w-full h-[35vh] sm:h-[45vh] md:h-[60vh] lg:h-[70vh] object-cover"
           />
         </div>
+
         <div>
           <img
             src={banner_2}
             alt="Smartwatch Banner"
-            className="w-full h-[60vh] object-cover"
+            className="w-full h-[35vh] sm:h-[45vh] md:h-[60vh] lg:h-[70vh] object-cover"
           />
         </div>
+
         <div>
           <img
             src={banner_3}
             alt="Appliances Banner"
-            className="w-ful h-[60vh] object-cover"
+            className="w-full h-[35vh] sm:h-[45vh] md:h-[60vh] lg:h-[70vh] object-cover"
           />
         </div>
       </Slider>
