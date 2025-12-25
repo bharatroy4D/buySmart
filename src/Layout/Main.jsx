bcc4a5a4bf6c0455ec9ca26a3412073d5f1c254c
bharatroy4D <bharatroy1005@gmail.com>
2025-12-25 22:30:59 +0600

    ScrollToTop component makeing and setting

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Navbar from '../Shared/Navbar';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Shared/Footer';
+import ScrollToTop from '../Components/ScrollToTop/ScrollToTop';
 
 const Main = () => {
     return (
         <div>
+            <ScrollToTop />
             <Navbar />
             <Outlet />
-            <Footer/>
+            <Footer />
         </div>
     );
 };
